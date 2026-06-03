c7bc1197790b62ec468e297590a98b61e41105b4
Ricardo Nunes <ricardo.agn.nunes@gmail.com>
2026-06-03 14:58:24 +0100

    Retry GitHub Pages deploy

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 window.GUESTBOOK_CONFIG = {
-  APP_URL: "https://YOUR-USERNAME.github.io/YOUR-REPOSITORY/",
+  APP_URL: "https://ricardonetanunes.github.io/guestbook-expo/",
   SUPABASE_URL: "https://urjfwikigdrgjslztnwu.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_MQ_Ff7dQ_uYkTAwIi5eyew_vNJeC2-g"
 };
